de914a496722716d9b47b928b5ec5c087e3e4032
finleywood <finleyjohnwood@gmail.com>
2021-01-12 18:34:31 +0000

    First Commit

---
+++ client/intuicards/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+      </nav>
+      <div className="container">
+        <br />
+        <h1>Hello World!</h1>
+        <button className="btn btn-primary">Test Button</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
